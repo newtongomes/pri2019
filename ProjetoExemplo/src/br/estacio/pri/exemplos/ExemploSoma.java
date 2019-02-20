7d13d9aef1eb0fb4b30a9b0874d3294cc2dd275e
newto <newto@NEW-ACER-2018>
2019-02-20 20:47:23 -0300

    Projeto Exercicios

---
+++ ProjetoExemplo/src/br/estacio/pri/exemplos/ExemploSoma.java
@@ -0,0 +1,24 @@
+package br.estacio.pri.exemplos;
+
+import java.util.Scanner;
+
+public class ExemploSoma
+{
+	public static void main(String[]args)
+	{
+		//declaração das variáveis
+		int numero1, numero2, soma;
+
+		//declaração do objeto Scanner
+		Scanner sc = new Scanner(System.in);
+
+		//recebe pelo teclado dois números inteiros
+		System.out.print("Entre com o primeiro número inteiro: ");
+		numero1 = sc.nextInt();
+		System.out.print("Entre com o segundo número inteiro: ");
+		numero2 = sc.nextInt();
+
+		soma = numero1 + numero2;
+		System.out.printf("A soma de %d e %d resulta em %d\n",numero1, numero2, soma);
+	}
+}
---
+++ ProjetoExemplo/src/br/estacio/pri/exemplos/OlaMundo.java
@@ -0,0 +1,17 @@
+package br.estacio.pri.exemplos;
+
+public class OlaMundo {
+	/**
+	 * Método que executa o programa
+	 * public = É visto em qualquer lugar da aplicação
+	 * static = é iniciado automaticamente pela JVM, sem precisar de uma instância
+	 * void = Método sem retorno (retorno vazio)
+	 * main = Nome do método, que é obrigatorio ser este. Recebe como parâmetro um array de String.
+	 * String[] args = Array de argumentos que podem ser repassados na chamada do programa.
+	 */
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Olá, Mundo!"); //Imprime na tela a frase  
+	}
+}	
